c678cc88c1f78be996b48009cfae2da96c3dad54
Adam Essmyer <acemyer@gmail.com>
2021-02-03 08:07:13 -0600

    created exercise 2 and 3

--- src/main.go
+++ src/main.go
@@ -1,19 +1,16 @@
 package main
 
-import (
-	"fmt"
-)
-
-var a int
-
-type hamburger int
-
-var b hamburger
+import "fmt"
 
 func main() {
-	a = 42
-	b = 43
-	fmt.Println(a)
-	fmt.Println(b)
-	a = int(b)
+
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println(x, y, z)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+
 }
