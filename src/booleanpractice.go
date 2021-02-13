845e8f496a0471676d66ffb7b9f2d050c3d5d870
Adam Essmyer <acemyer@gmail.com>
2021-02-13 11:01:47 -0600

    bool practice

---
+++ src/booleanpractice.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+var x bool
+
+func main() {
+
+	a := 10
+	b := 42
+
+	fmt.Println(a == b)
+	fmt.Println(a != b)
+
+	fmt.Println(x)
+	x = true
+	fmt.Println(x)
+}
