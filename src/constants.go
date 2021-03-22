16b09fc2bbe697f1d1129d9c12a73707f0cb62cc
Adam Essmyer <acemyer@gmail.com>
2021-03-22 15:03:29 -0500

    practice with making constants

---
+++ src/constants.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+const a = 007
+const b = "James Bond"
+
+func main() {
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Printf("%T\n", a)
+	fmt.Printf("$%T\n", b)
+
+}
